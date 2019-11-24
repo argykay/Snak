c4186bd3095932c8e9e47aac3e393232c692358e
argykay <argykonstantina@gmail.com>
2019-11-24 11:12:08 +0100

    works!

--- src/screens/TrialScreen/TrialScreen.js
+++ src/screens/TrialScreen/TrialScreen.js
@@ -26,8 +26,11 @@ class TrialScreen extends Component {
         <div className="background">
           <div className="mobile-container">
             <Star />
-            <h1 className="screen-title">CORRECT!</h1>
+            <h1 className="screen-title">That's correct!</h1>
             <p className="normal-text">Sign Up to keep playing!</p>
+            <div>
+              <img className="star-image star" src="./../images/star.svg" />
+            </div>
             <div>
               <button className="main-button" onClick={this.handleClick}>
                 SIGN UP
