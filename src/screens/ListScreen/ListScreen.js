a2d0760bbde0eff96cb0c03be8ff3b6a3b5e790e
argykay <argykonstantina@gmail.com>
2019-11-29 13:44:40 +0100

    done

--- src/screens/ListScreen/ListScreen.js
+++ src/screens/ListScreen/ListScreen.js
@@ -47,7 +47,8 @@ class ListScreen extends Component {
                       className="danish"
                       onClick={() => word.pronounciation.play()}
                     >
-                     <img className="play-svg" src="./../images/play.svg"/> {word.wordDanish} 
+                      <img className="play-svg" src="./../images/play.svg" />{" "}
+                      {word.wordDanish}
                     </td>
                     <td>{word.wordEnglish}</td>
                   </tr>
